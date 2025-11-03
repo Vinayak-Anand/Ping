81c164a5911b6db413b5de77e6b1330bd0150afa
Vinayak Anand <vinayakanand010503@gmail.com>
2025-11-04 00:25:29 +0530

    initial setup

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+app.use(express.json());
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
